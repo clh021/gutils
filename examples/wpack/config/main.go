09e05671c7ae2d049d7419ee5a2696bfc10542fd
chenlianghong <clh021@gmail.com>
2024-07-04 16:12:23 +0800

    wpack: clean config example

--- examples/wpack/config/main.go
+++ examples/wpack/config/main.go
@@ -27,6 +27,4 @@ func main() {
 
 	// 使用 spew.Dump 打印配置结构体，它提供了更详细的输出
 	spew.Dump(conf)
-	// 或者使用 fmt.Printf 格式化输出
-	// fmt.Printf("Config: PrometheusAddress=%s, Port=%d\n", conf.PrometheusAddress, conf.Port)
 }
